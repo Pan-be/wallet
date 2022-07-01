618fd69515788adcae28170b087f8ea23b6beacf
Konrad Bieniek <konradbieniek@gmail.com>
2022-07-01 15:37:21 +0200

    very first querrySelectors

---
+++ script.js
@@ -0,0 +1,15 @@
+const incomeSection = document.querySelector('.income-area')
+const expensesSection = document.querySelector('.expenses-area')
+const availableMoney = document.querySelector('.available-money')
+const addTransactionPanel = document.querySelector('.add-transaction-panel')
+
+const nameInput = document.querySelector('#name')
+const amountInput = document.querySelector('#amount')
+const categorySelect = document.querySelector('#category')
+
+const addTransactionBtn = document.querySelector('.add-transaction')
+const deleteAllBtn = document.querySelector('.delete-all')
+const saveBtn = document.querySelector('.save')
+const cancelBtn = document.querySelector('.cancel')
+const deleteBtn = document.querySelector('.delete')
+
